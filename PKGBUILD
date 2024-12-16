25de6a4e815f282c3310441ab08d77566c07ea23
Ardox <ardox@axos-project.com>
2024-12-16 08:57:15 +0100

    Attenpt to set default wallpaper

--- PKGBUILD
+++ PKGBUILD
@@ -1,5 +1,5 @@
 pkgname="axskel-hypr"
-pkgver="2.0"
+pkgver="2.1"
 pkgrel="1"
 pkgdesc="skel configs and looks for AxOS Hypr"
 arch=("x86_64")
--- src/.config/ags/services/messages.js
+++ src/.config/ags/services/messages.js
@@ -52,8 +52,8 @@ export async function firstRunWelcome() {
             .then(() => {
                 // Note that we add a little delay to make sure the cool circular progress works
                 Utils.execAsync(['hyprctl', 'keyword', 'bind', "Super,Slash,exec,ags -t cheatsheet"]).catch(print);
-                Utils.execAsync(['bash', '-c', `sleep 0.5; notify-send "Millis since epoch" "$(date +%s%N | cut -b1-13)"; sleep 0.5; notify-send '${FIRST_RUN_NOTIF_TITLE}' '${FIRST_RUN_NOTIF_BODY}' -a '${APP_NAME}' &` ]).catch(print)
-                Utils.execAsync(['bash', '-c', 'sleep 0.5;', '~/.config/ags/scripts/color_generation/switchwall.sh', '--path', '/usr/share/backgrounds/aesthetic_deer.jpg', '&']).catch(print);
+                Utils.execAsync(['bash', '-c', `sleep 0.5; notify-send '${FIRST_RUN_NOTIF_TITLE}' '${FIRST_RUN_NOTIF_BODY}' -a '${APP_NAME}' &` ]).catch(print)
+                Utils.execAsync(['bash', '-c', `sleep 0.5; sh ${GLib.get_user_config_dir()}/ags/scripts/color_generation/switchwall.sh`, '--path', '/usr/share/backgrounds/aesthetic_deer.jpg', '&']).catch(print);
             })
             .catch(print);
     }
